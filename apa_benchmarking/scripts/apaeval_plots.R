1dd29b688a5c0af4c428f550975cd118131b3877
SamBryce-Smith <samuelbrycesmith@gmail.com>
2024-04-12 14:24:26 +0100

    add function to rank methods by median score. Output TSVs with stats and ranks

--- apa_benchmarking/scripts/apaeval_plots.R
+++ apa_benchmarking/scripts/apaeval_plots.R
@@ -100,6 +100,47 @@ summarise_metrics <- function(df,
 }
 
 
+#' Rank Metrics values (1..n in highest-lowest) within Data Frame Groups
+#'
+#' This function ranks rows within groups of a data frame based on a specified column. Mainly intended to be applied to the output of summarise_metrics
+#'
+#' @param df A data frame containing metrics to be ranked.
+#' @param group_cols Character vector specifying the columns used for grouping the data frame. Defaults to c("window_size", "metric", "dataset")
+#' @param rank_by A string specifying the column name to use for ranking within each group. Defaults to "metrics.value.median"
+#'   This should be a column containing the metric values used for ranking.
+#' @param rank_outcol A string specifying the name of the new column to store the ranks.
+#'   Defaults to "rank".
+#'
+#' @return A modified data frame with an additional column containing the ranks within each group.
+#'
+#' @import dplyr
+#'
+#' @examples
+#' # Example usage:
+#' data <- data.frame(
+#'   window_size = c(10, 20, 10, 20, 10, 20),
+#'   metric = c("A", "A", "B", "B", "C", "C"),
+#'   dataset = c("X", "X", "X", "X", "Y", "Y"),
+#'   value = c(5, 10, 3, 8, 15, 20)
+#' )
+#'
+#' # Rank by 'value' within groups defined by 'window_size', 'metric', and 'dataset':
+#' ranked_data <- rank_metrics(data, group_cols = c("window_size", "metric", "dataset"), rank_by = "value")
+#'
+#' @export
+rank_metrics <- function(df,
+                         group_cols = c("window_size", "metric", "dataset"),
+                         rank_by = "metrics.value.median",
+                         rank_outcol = "rank") {
+  
+  df %>%
+    group_by(across(all_of(group_cols))) %>%
+    arrange(desc(!!sym(rank_by))) %>%
+    mutate("{rank_outcol}":= min_rank(desc(!!sym(rank_by)))) %>%
+    ungroup()
+}
+
+
 # identification challenge results
 id_df <- read_tsv("data/metrics_01.tsv")
 # absolute (TPM) quantification challenge results
@@ -112,20 +153,58 @@ absquant0_df <- clean_apaeval_df(absquant0_df)
 absquant1_df <- clean_apaeval_df(absquant1_df)
 
 # for each participant, dataset, window_size + metric, calculate the median + IQR 
-id_df_summ <- summarise_metrics(id_df)
-absquant0_df_summ  <- summarise_metrics(absquant0_df)
-absquant1_df_summ  <- summarise_metrics(absquant1_df)
+id_df_summ <- summarise_metrics(id_df) %>%
+  rank_metrics()
+absquant0_df_summ  <- summarise_metrics(absquant0_df) %>%
+  rank_metrics()
+absquant1_df_summ  <- summarise_metrics(absquant1_df) %>%
+  rank_metrics()
+
+# subset to experimental datasets only - calculate global median + IQR
+id_df_summ_exp <- id_df %>%
+  filter(datatype == "experimental") %>%
+  summarise_metrics(group_cols = c("participant_id", "window_size", "metric")) %>%
+  rank_metrics(group_cols = c("window_size", "metric"))
+
+absquant0_df_summ_exp  <- absquant0_df %>%
+  filter(datatype == "experimental") %>%
+  summarise_metrics(group_cols = c("participant_id", "window_size", "metric")) %>%
+  rank_metrics(group_cols = c("window_size", "metric"))
+
+absquant1_df_summ_exp  <- absquant1_df %>%
+  filter(datatype == "experimental") %>%
+  summarise_metrics(group_cols = c("participant_id", "window_size", "metric")) %>%
+  rank_metrics(group_cols = c("window_size", "metric"))
 
 # Where metrics overlap, visualise de-novo and annotation-based tools together
 common_metrics_idquant <- intersect(unique(id_df$metric), unique(absquant0_df$metric))
 
 # Combined df for common metrics of de-novo and annot-based tools, removing APAtrap*1 which is evaluated in both challenges
+# Add ranks considering combo of de-novo and reference-based tools
 id_absquant0_df_summ <- bind_rows(filter(id_df_summ, metric %in% common_metrics_idquant),
                                   filter(absquant0_df_summ, metric %in% common_metrics_idquant & str_starts(participant_id, "APAtrap", negate = T))
-                                  )
+                                  ) %>%
+  rank_metrics(rank_outcol = "rank.combined")
+
 id_absquant1_df_summ <- bind_rows(filter(id_df_summ, metric %in% common_metrics_idquant),
                                   filter(absquant1_df_summ, metric %in% common_metrics_idquant & str_starts(participant_id, "APAtrap", negate = T))
-)
+                                  ) %>%
+  rank_metrics(rank_outcol = "rank.combined")
+
+
+# Repeat combining with median across all experimental data sets
+
+id_absquant0_df_summ_exp <- bind_rows(filter(id_df_summ_exp, metric %in% common_metrics_idquant),
+                                  filter(absquant0_df_summ_exp, metric %in% common_metrics_idquant & str_starts(participant_id, "APAtrap", negate = T))
+) %>%
+  rank_metrics(group_cols = c("window_size", "metric"),
+               rank_outcol = "rank.combined")
+
+id_absquant1_df_summ_exp <- bind_rows(filter(id_df_summ_exp, metric %in% common_metrics_idquant),
+                                  filter(absquant1_df_summ_exp, metric %in% common_metrics_idquant & str_starts(participant_id, "APAtrap", negate = T))
+) %>%
+  rank_metrics(group_cols = c("window_size", "metric"),
+               rank_outcol = "rank.combined")
 
 
 # Vector of different window sizes for matching PAS
@@ -182,3 +261,48 @@ walk2(id_heatmaps,
                width = 11.7,
                height = 8.3, units = "in")
 )
+
+
+# Output summary TSVs with median and ranks
+# by dataset
+# summarised by median across experimental
+# Combined with reference-based tools
+
+# id_absquant0_df_summ 
+# id_absquant1_df_summ
+# id_absquant0_df_summ_exp
+# id_absquant1_df_summ_exp
+
+write_tsv(id_absquant0_df_summ, file.path("processed", paste0("2024-04-12_identification_denovo_reference",
+                                        ".mintpm_0",
+                                        ".metric_medians",
+                                        ".per_dataset",
+                                        ".tsv"))
+          )
+
+
+write_tsv(id_absquant1_df_summ, file.path("processed", paste0("2024-04-12_identification_denovo_reference",
+                                                              ".mintpm_1",
+                                                              ".metric_medians",
+                                                              ".per_dataset",
+                                                              ".tsv"))
+          )
+
+
+write_tsv(id_absquant0_df_summ_exp, file.path("processed", paste0("2024-04-12_identification_denovo_reference",
+                                                              ".mintpm_0",
+                                                              ".metric_medians",
+                                                              ".experimental_global",
+                                                              ".tsv"))
+)
+
+write_tsv(id_absquant1_df_summ_exp, file.path("processed", paste0("2024-04-12_identification_denovo_reference",
+                                                                  ".mintpm_1",
+                                                                  ".metric_medians",
+                                                                  ".experimental_global",
+                                                                  ".tsv"))
+)
+
+
+          
+    
